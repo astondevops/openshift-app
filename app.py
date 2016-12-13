aff14ed2beaa3683e2f4cbf4e11adf0a6d8a22b6
formateur <astondevops@gmail.com>
2016-12-13 16:37:15 +0100

    port changed

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=int(os.getenv('PORT', 8080)))
